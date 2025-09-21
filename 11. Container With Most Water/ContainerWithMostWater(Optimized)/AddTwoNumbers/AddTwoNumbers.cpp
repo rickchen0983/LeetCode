c8d7d06d621db6a946a5bb93907b7cca0bb36027
rickchen0983 <r2606325@gmail.com>
2025-09-21 12:55:21 +0800

    Issue #3 [LeetCode] 11. Container With Most Water
    - Added answer

---
+++ 11. Container With Most Water/ContainerWithMostWater(Optimized)/AddTwoNumbers/AddTwoNumbers.cpp
@@ -0,0 +1,52 @@
+﻿// MergeTwoSortedLists.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+
+int maxArea(int* height, int heightSize);
+
+int main()
+{
+	int maxAreaValue = 0;
+	int height[] = { 1,8,6,2,5,4,8,3,7 };
+	maxAreaValue = maxArea(height, sizeof(height) / sizeof(height[0]));
+}
+
+int maxArea(int* height, int heightSize) 
+{
+    int left = 0;
+    int right = heightSize - 1;
+    int maxArea = 0;
+
+    while (left < right) {
+        int h = height[left] < height[right] ? height[left] : height[right];
+        int width = right - left;
+        int area = h * width;
+
+        if (area > maxArea) {
+            maxArea = area;
+        }
+
+        // 移動較矮的那邊
+        if (height[left] < height[right]) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+    return maxArea;
+}
+
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
